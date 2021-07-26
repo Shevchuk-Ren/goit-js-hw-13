c66c359b13c11334489bc33017a8c781ce9ed549
Shevchuk-Ren <shevchuk_re@ukr.net>
2021-07-27 01:01:07 +0300

    fix rendering pictures

--- src/index.js
+++ src/index.js
@@ -4,14 +4,9 @@ import Notiflix from "notiflix";
 import gallery from './templates/gallery-card.hbs'
 import loadMoreBtn from './templates/button-load-more.hbs'
 
-// const options = {
-//     headers: {
-//         Authorization: '22611129-58a3168a9d70d9c0808a9c973',
-//     },
-    
-// };
-// console.log(NewsApiService)
+
 const newsApiService = new NewsApiService();
+
 
 const refs = {
     form: document.querySelector('.search-form'),
@@ -20,14 +15,7 @@ const refs = {
     galleryContainer: document.querySelector('.gallery'),
     // btnLoadMore: document.querySelector('.load-more')
 }
-// const URL = `https://pixabay.com/api/?key=${options.headers.Authorization}&q=cat`;
-// fetch(URL).then(r => 
-//     r.json()
-// ).then(console.log);
-// console.log(console.log)
 
-// refs.btnSearch.addEventListener('click', searchGalleryImg);
-// refs.input.addEventListener('input', onInput)
 refs.form.addEventListener('submit', onSearch)
 
 
@@ -35,20 +23,21 @@ refs.form.addEventListener('submit', onSearch)
 
 function onSearch(evt) {
     evt.preventDefault()
-     
+     clearGallery()
     // через форму добираемся до инпута по его имени searchQuery делаем потому что  refs.input.value при модульном хранении файлов не работает
     newsApiService.query = evt.currentTarget.elements.searchQuery.value;
     newsApiService.resetPage();
+   
     
     newsApiService.fetchArticles().then(foto => {
-    
+      
      appendGalleryMarkup(foto)   
     })
 
     if (document.querySelector('.load-more') === null) {
         console.log(`first`)
          const loadMoreButton = loadMoreBtn();
-        refs.galleryContainer.insertAdjacentHTML('beforeend', loadMoreButton);
+        refs.galleryContainer.insertAdjacentHTML('afterend', loadMoreButton);
         
     }
      console.log(`second`)
@@ -66,8 +55,7 @@ function onLoadMore() {
    
      appendGalleryMarkup(foto)   
     })
-   const btnLoadMore = document.querySelector('.load-more')
-    btnLoadMore.addEventListener('click', onLoadMore);
+  
     
   
 }
