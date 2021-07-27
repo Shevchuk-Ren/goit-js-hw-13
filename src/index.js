9df582c4c95aa3c817e743ff4ca1b0fe9bd99b8c
Shevchuk-Ren <shevchuk_re@ukr.net>
2021-07-27 16:30:45 +0300

    create search, add modalwindow

--- src/index.js
+++ src/index.js
@@ -3,66 +3,93 @@ import NewsApiService from './js/api-service'
 import Notiflix from "notiflix";
 import gallery from './templates/gallery-card.hbs'
 import loadMoreBtn from './templates/button-load-more.hbs'
+import LoadMoreBtnClass from './js/load-btn';
 
-
+// const loadMoreBtnClass = new LoadMoreBtnClass({
+//   selector: '.load-more',
+//   hidden: true,
+// });
 const newsApiService = new NewsApiService();
-
+//  console.log(loadMoreBtnClass)
 
 const refs = {
     form: document.querySelector('.search-form'),
     input: document.querySelector('input'),
     btnSearch: document.querySelector('button'),
     galleryContainer: document.querySelector('.gallery'),
-    // btnLoadMore: document.querySelector('.load-more')
 }
 
 refs.form.addEventListener('submit', onSearch)
 
-
+let sum = null;
+let btnLoadMore = null;
 
 
 function onSearch(evt) {
     evt.preventDefault()
-     clearGallery()
+   
     // через форму добираемся до инпута по его имени searchQuery делаем потому что  refs.input.value при модульном хранении файлов не работает
     newsApiService.query = evt.currentTarget.elements.searchQuery.value;
     newsApiService.resetPage();
    
-    
-    newsApiService.fetchArticles().then(foto => {
-      
-     appendGalleryMarkup(foto)   
-    })
+   
+    newsApiService.fetchFoto().then(({ hits, totalHits }) => {
+        sum = hits.length;
 
-    if (document.querySelector('.load-more') === null) {
-        console.log(`first`)
-         const loadMoreButton = loadMoreBtn();
-        refs.galleryContainer.insertAdjacentHTML('afterend', loadMoreButton);
-        
-    }
-     console.log(`second`)
-       const btnLoadMore = document.querySelector('.load-more')
-    btnLoadMore.addEventListener('click', onLoadMore);
-  
-    
+        console.log(totalHits)
+        if (totalHits === 0) {
+              clearGallery()
+      return  Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+     }
+      Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+     appendGalleryMarkup(hits, totalHits)   
+    })
+ 
 
 }
 
 function onLoadMore() {
    
-      clearGallery()
-    newsApiService.fetchArticles().then(foto => {
-   
-     appendGalleryMarkup(foto)   
-    })
+
+    newsApiService.fetchFoto().then(({hits, totalHits}) => {
+        sum += hits.length;
+          console.log(sum, `sum`)
+        appendGalleryMarkup(hits, totalHits)
+        
+    }).catch()
   
     
   
 }
-function appendGalleryMarkup(foto) {
-    console.log(foto, `foto`)
-    refs.galleryContainer.insertAdjacentHTML('afterbegin', gallery(foto))
+function appendGalleryMarkup(foto, totalHits) {
+
+    clearGallery()
+    console.log(totalHits, `hits`)
+   
+ 
     
+      if (document.querySelector('.load-more') === null) {
+          console.log(btnLoadMore, `loadd`);
+          
+          const loadMoreButton = loadMoreBtn();
+          
+          refs.galleryContainer.insertAdjacentHTML('beforeend', loadMoreButton);
+          
+           
+          btnLoadMore = document.querySelector('.load-more');
+          
+    
+        
+      } 
+    btnLoadMore.addEventListener('click', onLoadMore);
+   
+    
+    refs.galleryContainer.insertAdjacentHTML('afterbegin', gallery(foto))
+      if (sum >= totalHits) {
+           
+            btnLoadMore.remove(); 
+             return  Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+        }
 }
 function clearGallery() {
     refs.galleryContainer.innerHTML = '';
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,3 +1,12 @@
+const API_KEY = '22611129-58a3168a9d70d9c0808a9c973';
+const BASE_URL = 'https://pixabay.com/api/'
+const options = {
+    headers: {
+        Authorization: API_KEY,
+    },
+   
+};
+        
 export default class NewsApiService {
     constructor() {
         this.querySearch = '';
@@ -5,31 +14,29 @@ export default class NewsApiService {
     
     }
     //метод отвечает за все http запросы
-    fetchArticles() {
+    fetchFoto() {
 
-    const options = {
-    headers: {
-        Authorization: '22611129-58a3168a9d70d9c0808a9c973',
-    },
-   
-        };
+    
         console.log(`option`, this.querySearch)
-         console.log(`page`, this.page)
-         const url = `https://pixabay.com/api/?key=${options.headers.Authorization}&q=${this.querySearch}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=4`;
+         console.log(`page`, this)
+         const url = `${BASE_URL}?key=${options.headers.Authorization}&q=${this.querySearch}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`;
  return fetch(url).then(r => 
     r.json()
 ).then(data => {
-   
+    console.log(data, `pageper`)
     this.incrementPage()
-    return data.hits;
+    // return data.hits;
+    return data;
     });
     }
     
     incrementPage() {
         this.page += 1;
+        // this.per_page += 40;
     }
     resetPage() {
         this.page = 1;
+        //  this.per_page = 40;
     }
     get query() {
         return this.querySearch;
